76297d7e3c69e23f61bda863a9aac17f6611093f
Ben Hughes <me@benhughes.name>
2020-01-26 13:09:40 -0800

    Include build directory in git for easy dokku git deploys with .static

---
+++ public/component---src-pages-404-js-8a182a82cc3e6057aae3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a);t.default=function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement("h1",null,"404"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8a182a82cc3e6057aae3.js.map
---
+++ public/styles-f86ce7ec4d994ef701f7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"0sYV":function(n,o,p){},"8ypT":function(n,o,p){}}]);
+//# sourceMappingURL=styles-f86ce7ec4d994ef701f7.js.map
